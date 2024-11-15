1d1cae4486c2698cc9553ebac7a7586bbdd2715f
Nicolay <nicolaykblondin@getacademy.no>
2024-11-15 10:29:41 +0000

    if else task 1 (x == y)

--- module/Tasks/3-IfElse/Task-1.cs
+++ module/Tasks/3-IfElse/Task-1.cs
@@ -1,2 +1,12 @@
 using System;
 
+//Lag en metode som tar imot to tall og returnerer true dersom tallene er like.
+
+static void main(string[]args)
+{
+    int x = 5;
+    int y = 99;
+
+    if (x == Y)
+    
+}
