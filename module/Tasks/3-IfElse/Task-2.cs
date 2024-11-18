060189b129a32f3a4075e45b94fdf470bea37a7c
Nicolay <nicolaykblondin@getacademy.no>
2024-11-18 11:15:40 +0000

    task 2 create a method that returns the sum if its != and multiply it if they are ==

--- module/Tasks/3-IfElse/Task-2.cs
+++ module/Tasks/3-IfElse/Task-2.cs
@@ -1,22 +1,22 @@
 using System;
 
 // Lag en metode som tar imot to tall og returnerer summen av tallene dersom de er ulike, 
-//og returnerer tallene multiplisert med hverandre dersom de er like
+og returnerer tallene multiplisert med hverandre dersom de er like
 
 class CCXVI
 {
     static void Main(string[] args)
     {
-        int x = 8;
+        int x = 9;
         int y = 9;
 
         if (x == y)
         {
-            Console.WriteLine(x * y);
+            console.writeLine(x * y);
         }
         else
         {
-            Console.WriteLine(x + y);
+            console.writeLine(x + y);
         }
     }
 }
--- module/Tasks/Program runner/SumMethodApp/obj/Debug/net8.0/SumMethodApp.AssemblyInfo.cs
+++ module/Tasks/Program runner/SumMethodApp/obj/Debug/net8.0/SumMethodApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SumMethodApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d1cae4486c2698cc9553ebac7a7586bbdd2715f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2ffdd6b6aa36538d20c9c612b68e4587fd4ddae")]
 [assembly: System.Reflection.AssemblyProductAttribute("SumMethodApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SumMethodApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
