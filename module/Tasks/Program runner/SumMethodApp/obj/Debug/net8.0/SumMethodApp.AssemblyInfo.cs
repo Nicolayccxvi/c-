26a1b4cd3b5b7fcd5e05847789cebddd40f9d1a1
Nicolay <nicolaykblondin@getacademy.no>
2024-11-29 10:28:24 +0000

    LivePetParProg

--- module/Tasks/Program runner/SumMethodApp/obj/Debug/net8.0/SumMethodApp.AssemblyInfo.cs
+++ module/Tasks/Program runner/SumMethodApp/obj/Debug/net8.0/SumMethodApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SumMethodApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59b55d909ac34c62678da09af08cda43cb9ab2be")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1bc94681a0a719d8342bd5cbd34413302caf8933")]
 [assembly: System.Reflection.AssemblyProductAttribute("SumMethodApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SumMethodApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
